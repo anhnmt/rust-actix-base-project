868956448b1765040c793e972bc8b00315a43904
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-04-09 09:23:17 +0700

    Update log

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ use std::io;
 
 use actix_web::{App, HttpServer, middleware};
 use dotenvy::dotenv;
+use fast_log::config::Config;
+use fast_log::consts::LogSize;
+use fast_log::plugin::file_split::RollingType;
+use fast_log::plugin::packer::LogPacker;
 use log::info;
+use middleware::Logger;
 
 // External modules reference
 mod router;
@@ -12,13 +17,19 @@ mod router;
 #[tokio::main]
 async fn main() -> io::Result<()> {
     dotenv().ok();
-    env_logger::init();
+    fast_log::init(Config::new()
+        .console()
+        .file_split("logs/",
+                    LogSize::MB(1),
+                    RollingType::All,
+                    LogPacker {})
+    ).unwrap();
 
     info!("starting HTTP server at http://localhost:8080");
 
     HttpServer::new(|| {
         App::new()
-            .wrap(middleware::Logger::default())
+            .wrap(Logger::new(r#"%a "%r" %s %b "%{Referer}i" "%{User-Agent}i" %T %D"#))
             .configure(router::init)
     })
         .bind(("127.0.0.1", 8080))?
